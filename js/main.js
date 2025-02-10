c7b7c407b3818ae7447f29a8cd7cfce98ded9610
rakellbandeira <per22016l@byui.edu>
2025-02-10 19:43:05 -0400

    header and footer template inside main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+const headerTemplate = `
+    <header>
+    
+        <div class="left-area">
+          <div class="menu">
+            <div class="menu-option" id="home-option" onclick="toggleDisplay('recipe-area')">
+              <img src="./assets/images/home-icon.png" alt="Home">
+              <span>Home</span>
+            </div>
+            
+            <div class="menu-option" id="pantry-option" onclick="toggleDisplay('pantry')">
+              <img src="./assets/images/fridge-icon.png" alt="My Pantry">
+              <span>My Pantry</span>
+              
+            </div>
+          </div>
+    
+          
+        </div>
+        <div class="header-title">       
+          <div class="title">
+            <h1>Recipe Wizard |</h1>
+            <h3>Your fastest recipe finder</h3>
+          </div>
+
+          <div class="favoritesButton">
+            <a><img src="" alt="heart">See Favorites</a>
+          </div>
+        </div>
+
+    
+</header>
+`;
+
+const footerTemplate = `
+    <footer>
+    <p>WDD 330: Web Frontend Development II - Final Project</p>
+    <p>Rakell Bandeira - 2025</p>
+</footer>
+`;
+
+
+
+
 document.addEventListener('DOMContentLoaded', async () => {
     const edamamAPI = new EdamamAPI();
     const foodFactsAPI = new OpenFoodFactsAPI();
@@ -24,11 +68,11 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 async function loadHeaderAndFooter() {
-    const headerResponse = await fetch ("../js/public/header.html");
-    const headerHtml = await headerResponse.text();
-    document.body.insertAdjacentHTML('afterbegin', headerHtml);
+    /* const headerResponse = await fetch ("../js/public/header.html");
+    const headerHtml = await headerResponse.text(); */
+    document.body.insertAdjacentHTML('afterbegin', headerTemplate);
 
-    const footerResponse = await fetch ("../js/public/footer.html");
-    const footerHtml = await footerResponse.text();
-    document.body.insertAdjacentHTML('beforeend', footerHtml);
+    /* const footerResponse = await fetch ("../js/public/footer.html");
+    const footerHtml = await footerResponse.text(); */
+    document.body.insertAdjacentHTML('beforeend', footerTemplate);
 }
